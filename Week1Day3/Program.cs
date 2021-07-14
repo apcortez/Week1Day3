3183b50d2b84817ff837ff3a655ab2a702486f3d
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-14 09:27:45 +0200

    Add project files.

---
+++ Week1Day3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week1Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //routine
+            Menu();
+
+
+
+
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("***MENU***");
+            Console.WriteLine("1. Opzione1");
+            Console.WriteLine("2. Opzione2");
+            Console.WriteLine("3. Opzione3");
+        }
+    
+    }
+}
