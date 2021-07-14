4a454810c42a7e01fd79c1611f2a42adf190e7c3
Angelica Cortez <angelica.cortez@avanade.com>
2021-07-14 10:07:08 +0200

    metodi con passaggio per valore e per riferimento

--- Week1Day3/Program.cs
+++ Week1Day3/Program.cs
@@ -9,12 +9,50 @@ namespace Week1Day3
             //routine
             Menu();
 
+            //metodo con parametri per la somma
             int a = 1;
             int b = 2;
             int somma = 0;
             somma = Sum(a, b);
 
+            //metodo con parametri per la somma e successivamente la sua stampa
+            int c = 40;
+            int d = 4;
+            int sommaCD = Sum(c, d);
+            Console.WriteLine(sommaCD);
 
+            //metodo con parametri per la stampa
+            int e = 45;
+            int f = 2;
+            StampaSomma(e, f);
+
+            int var1 = 10;
+            Cambiovalore(var1); //NON cambia valore var1 alla stampa rimane sempre 10 perchè sta solo nel metodo --> passaggio di parametro per valore
+            Console.WriteLine($"Il valore di var1 dopo la chiamata a Cambiovalore è {var1}");
+            
+            CambiovalorePerRif(ref var1); //cambia il valore di var1 alla stampa --> passaggio per riferimento
+            Console.WriteLine($"Il valore di var1 dopo la chiamata a CambiovalorePerRif è {var1}"); 
+
+        }
+
+        //Metodo con passaggio per valore
+        private static void Cambiovalore(int x)
+        { 
+            x = 5;
+            Console.WriteLine($"Ho cambiato il mio parametro. Valore = {x}");
+        }
+
+        //Metodo con passagio per riferimento
+        private static void CambiovalorePerRif(ref int x)
+        {
+            x = 5;
+            Console.WriteLine($"Ho cambiato il mio parametro. Valore = {x}");
+        }
+
+
+        private static void StampaSomma(int x, int y)
+        {
+            Console.WriteLine($"La somma tra {x} e {y} è {Sum(x,y)}");
         }
 
         private static int Sum(int x, int y)
